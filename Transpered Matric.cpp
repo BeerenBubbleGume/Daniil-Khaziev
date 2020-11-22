c63163a5ffff988a2276c95512a0114c6ad2375f
Daniil Khaziev <54493389+BeerenBubbleGume@users.noreply.github.com>
2020-11-22 12:17:51 +0500

    ewww

---
+++ Transpered Matric.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    const int   n = 10, m = 10;
+    int         i = 0, k = 0, y = 0, rev;
+
+    srand(2);
+
+    int** arr = new int* [n];
+    for (int i = 0; i < n; i++)
+    {
+        arr[i] = new int[m];
+    }
+
+    printf("Matric before transphorm: \n");
+    for (int i = 0; i < n; i++)
+    {
+        for (y = 0; y < m; y++)
+        {
+            arr[i][y] = rand() % 10;
+            cout << arr[i][y] << " ";
+        }
+        cout << endl;
+    }
+
+    printf("Matric after transphorm: \n");
+    for (i = 0; i < n / 2; i++)
+    {
+        for (y = 0; y < n / 2; y++)
+        {
+            k = arr[y][i];
+            arr[i][y] = arr[n - 1 - i][n - 1 - y];
+            arr[n - 1 - i][n - 1 - y] = k;
+        }
+    }
+    for (i = 0; i < n; i++)
+    {
+        for (y = 0; y < n; y++)
+        {
+            cout << arr[i][y] << " ";
+        }
+        cout << endl;
+    }
+    system("pause");
+    return 0;
+}
