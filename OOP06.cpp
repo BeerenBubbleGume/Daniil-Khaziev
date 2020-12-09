32a49c7e390729efc6b567eb43bdf8affcbf6147
Daniil Khaziev <54493389+BeerenBubbleGume@users.noreply.github.com>
2020-12-09 18:25:08 +0500

    ewww

--- OOP06.cpp
+++ OOP06.cpp
@@ -3,89 +3,85 @@
 #include <iostream>
 #include <cmath>
 #include <cstdlib>
+#include <string>
 
 using namespace std;
 
+struct complexNum{
+    double real,imag;
+};
 class ComplexNum{
     public:
-    double imagnum;
-    double num;
+    complexNum comp;
+    
+    ComplexNum(double x = 0,double y = 0){
+        comp.real = x;
+        comp.imag = y;
+    }
+    const ComplexNum& operator =(const ComplexNum &c){
+        comp.real = c.comp.real;
+        comp.imag = c.comp.imag;
+        return *this;
+    }
 
-    long double imcomplex(){
-        double s = 0;
-        int i = -1;
-        s += num + i * imagnum;
-        return s;
-    }
-    long double imsubstraction(){
-        double s = 0;
-        int i = -1;  
-            s += num - i * imagnum;
-        return s;
-    }
-    long double immulti(){
-        double s = 0;
-        int i = -1;
-            s += num * i * imagnum;
-        return s;
-    }
-    long double imdevis(){
-        double s = 0;
-        int i = -1; 
-            s += num / imagnum * i;
-        return s;
-    }
-    void setAll(double i, double n){
-        imagnum = i;
-        num = n;
-    }
-    void show(){
-        cout << "Summ x + yi = " << imcomplex() << endl;
-        cout << "Substraction x - yi = " << imsubstraction() << endl;
-        cout << "Multiple x * yi = " << immulti() << endl;
-        cout << "Devision x / yi = " << imdevis() << endl;
-    }
-    ComplexNum(){
-        setAll(0, 0);
-    }
-    ComplexNum(double x, double y){
-        setAll(x, y);
-    }
-};
-class trigonomform: public ComplexNum{
-    public:
+    ComplexNum operator +(const ComplexNum &c);
+    ComplexNum operator *(const ComplexNum &c);
+    ComplexNum operator -(const ComplexNum &c);
+    ComplexNum operator /(const ComplexNum &c);
 
-    double trig(){
-        double s = 0;
-        int i = -1; 
-            s = sqrt(pow(num, 2) + pow(imagnum, 2)) * cos(num) + i * sin(imagnum);
-        return s;
-    }
-    void setAll(double x, double y){
-        ComplexNum::setAll(x, y);
-    }
-    void show(){
-        cout << "triganform is: " << trig() << endl;
-    }
-    trigonomform(double x, double y){
-        setAll(x, y);
-    }
+    ComplexNum Read();
+        void print();
 };
+ComplexNum ComplexNum::operator +(const ComplexNum &c){
+    ComplexNum t;
+    t.comp.real = comp.real + comp.real;
+    t.comp.imag = comp.imag + comp.imag;
+    return t;
+    }
+ComplexNum ComplexNum::operator -(const ComplexNum &c){
+    ComplexNum d;
+    d.comp.real = comp.real - comp.real;
+    d.comp.imag = comp.imag - comp.imag;
+    return d;
+    }
+ComplexNum ComplexNum::operator *(const ComplexNum &c){
+    ComplexNum m;
+    m.comp.real = comp.real * comp.real - comp.imag * comp.imag;
+    m.comp.imag = comp.real + comp.imag + comp.imag + comp.real;
+    return m;
+    }
+ComplexNum ComplexNum::operator /(const ComplexNum &c){
+    ComplexNum s;
+    s.comp.real = (comp.real * comp.real + comp.imag * comp.imag) / (comp.real * comp.real + comp.imag * comp.imag);
+    s.comp.imag = (comp.real * comp.imag - comp.real * comp.imag) / (comp.real * comp.real + comp.imag * comp.imag);
+    return s;
+    }
+void ComplexNum::print (){
+    if(comp.imag >= 0)
+        cout << comp.real << "+" << comp.imag << "i\n";
+    else
+    {
+        cout << comp.real << comp.imag << "i\n";
+    } 
+}
+ComplexNum ComplexNum::Read(){
+    double x,y;
+    cout<<"Действительная часть: ";
+    cin>>x;
+    cout<<"Мнимая часть:       : ";
+    cin>>y;
+    ComplexNum A = ComplexNum(x,y);
+    return A;
+}
 int main(){
-    double x, y;
-
-    printf("Enter number:");
-    cin >> x;
-    printf("\nEnter imagenering number:");
-    cin >> y;
-    cout << endl;
-
-    ComplexNum objA(x, y);
-    trigonomform objB(x,y);
-
-    objA.show();
-    cout << endl;
-    objB.show();
+    ComplexNum oper1(2.0, 4.0), oper2(3.0, 6.0), result;
+    result = oper1 + oper2;
+    result.print();
+    result = oper1 - oper2;
+    result.print();
+    result = oper1 * oper2;
+    result.print();
+    result = oper1 / oper2;
 
     return 0;
 }
